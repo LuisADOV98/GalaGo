72fdb7ff6c7f0a0fd5d3b0a5531379f5f5746cd2
Cristina M <cristinamunoz@MacBook-Air-de-Cristina.local>
2023-09-19 02:25:45 +0200

    filtra solo mujer

--- GalaGo/src/app/pages/home/home.component.ts
+++ GalaGo/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Prenda } from 'src/app/models/prenda';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  public filtrado = false;
+  public mujerActive = false;
+  public prendas: Prenda[];
 
+  constructor(){
+    this.prendas = [
+      new Prenda("Uno", 10, "descip", "Alc", "nuevo","boda", false, false, true),
+      new Prenda("Dos", 20, "descip", "Mdr", "nuevo","boda", true, false, false),
+      new Prenda("Tres", 30, "descip", "Val", "nuevo","boda", false, true, false)
+    ]
+    console.log("filterMujer:",this.mujerActive);
+  }
+
+  filterMujer():void{
+    this.mujerActive = !this.mujerActive;
+    console.log("filterMujer:",this.mujerActive);
+
+  }
 }
+
+
+
