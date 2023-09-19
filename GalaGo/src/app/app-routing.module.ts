0ff58ac6344c2d956458863255c6b9b8e6bf4e26
JanetRW <jane_row@hotmail.com>
2023-09-19 22:56:44 +0200

    cambios dia1

--- GalaGo/src/app/app-routing.module.ts
+++ GalaGo/src/app/app-routing.module.ts
@@ -19,7 +19,8 @@ import { RegistroComponent } from './pages/registro/registro.component';
 import { Prenda } from './models/prenda';
 
 const routes: Routes = [
-  {path:"", component: LandingPageComponent},
+  //{path:"", component: LandingPageComponent},
+  {path:"", component: ChatComponent},
   {path:"card", component: CardComponent},
   {path:"footer", component: FooterComponent},
   {path:"header", component: HeaderComponent},
