3a8849dabdde966b6b5954eb442f53a62bc0f28c
Mabel <mabelsanromanf@gmail.com>
2023-09-23 21:34:19 +0200

    cambios comp card

--- GalaGo/src/app/component/card/card.component.ts
+++ GalaGo/src/app/component/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Prenda } from 'src/app/models/prenda';
 import { PerfilComponent } from 'src/app/pages/perfil/perfil.component';
@@ -8,18 +8,144 @@ import { PerfilComponent } from 'src/app/pages/perfil/perfil.component';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit{
+  //usamos el input para recibir datos de 1 prenda desde home
+  @Input() prenda: Prenda;
+  //usamos el input para recibir datos de 1 prenda desde perfil/mis prendas
+  @Input() editable: Boolean;
+  mostrarCorazon: boolean = true; //para que salga el corazon en las cards, 
+                      //x defecto true (LANDING = FALSE)
+
+  /* logueado = true; */
   
  
-  respuesta:Prenda[] = [];
+//   respuesta:Prenda[] = [];
 
-  groupedItems = [];
+//   groupedItems = [];
 
-  constructor(private router: Router){
-    
-  }
+   constructor(private router: Router){ 
+   }
 
   ngOnInit(): void {
+  }
+
+  verDetallePrenda(id:number){
+    /* detalle-prenda */
+    if (this.editable === false && this.router.url === '/landing-page' ||  this.router.url === '/') {
+      // Si estás en la página de inicio y la tarjeta no es editable,
+      // redirige a login
+      this.router.navigate(['/login']);
+    } else {
+      // si no, redirecciona a detalle prenda
+      this.router.navigate(['/detalle-prenda', id]);
+    }
+}
+
+//Si card es editable, redirige a editar (desde perfil)
+   redirigir(id:number){
+    if(this.editable){
+        this.router.navigate(["/editar-prenda"])
+    }
+        console.log(id);
+  }
+    // } else {
+    //   this.router.navigate(['/detalle-prenda'])
+    // }
+    //     console.log(id);
+    // }
+
+
+
+    verSoloFavoritos(){
+          //¿Qué id tiene el usuario? normalmente se guarda en localhost
+          // hacer una petición al servidor sobre que favoritos tiene
+          // con todas las ids de las prendas favoritas me las guardo en 
+          // borro respuesta y meto los favoritos en respuesta
+          
+          // borro esto
+         /*  this.respuesta = [];
+          this.groupedItems = []; */
+          // meto todas sus prendas favoritas aqui:
+         /*  this.respuesta = []; */
+      
+         /*  for(let i = 0; i < this.respuesta.length; i +=2){
+            this.groupedItems.push(this.respuesta.slice(i, i+2))
+           } */
+          
+        }
+
+  }
+//   ngOnInit(): void {
+//     this.verTodas();
+//   }
+
+//   addFavoritos(id:number){
+//     console.log(id);
+//   }
+
+//   verSoloFavoritos(){
+//     //¿Qué id tiene el usuario? normalmente se guarda en localhost
+//     // hacer una petición al servidor sobre que favoritos tiene
+//     // con todas las ids de las prendas favoritas me las guardo en 
+//     // borro respuesta y meto los favoritos en respuesta
+    
+//     // borro esto
+//     this.respuesta = [];
+//     this.groupedItems = [];
+//     // meto todas sus prendas favoritas aqui:
+//     this.respuesta = [];
+
+//     for(let i = 0; i < this.respuesta.length; i +=2){
+//       this.groupedItems.push(this.respuesta.slice(i, i+2))
+//      }
+    
+//   }
+
+//   verTodas(){
+//     this.respuesta = [
+//       {
+//         id: 1,
+//         titulo: "Traje boda",
+//         imagen: "../../../assets/cards/chica_azul.jpg",
+//         precio: 20
+//       },
+//       {
+//         id: 2,
+//         titulo: "Traje boda",
+//         imagen: "../../../assets/cards/chica_morado.png",
+//         precio: 20
+//       },
+//       {
+//         id: 3,
+//         titulo: "Traje boda",
+//         imagen: "../../../assets/cards/chica_rosa.png",
+//         precio: 20
+//       },
+//       {
+//         id: 4,
+//         titulo: "Traje boda",
+//         imagen: "../../../assets/cards/chico_blanco.png",
+//         precio: 20
+//       },
+  
+//     ];
+//      /* this.groupedItems = []; */
+//     // meto todas sus prendas aqui:
+//     /* this.respuesta = []; */
+//     for(let i = 0; i < this.respuesta.length; i +=2){
+//       this.groupedItems.push(this.respuesta.slice(i, i+2))
+//      }
+//   }
+
+
+// //PARA DETALLE-CARD
+//   verDetallePrenda(id:number){
+//     /* detalle-prenda */
+//     this.router.navigate(['/detalle-prenda', id]);
+//   }
+// }
+
+///////////////////////////////////////////////////////////////////////////////////////
 
     //PODEMOS COMPROBAR SI HAN PEDIDO SOLO FAVORITOS
     // O HAN PEDIDO TODOS 
@@ -44,8 +170,7 @@ export class CardComponent implements OnInit {
     this.groupedItems.push(this.respuesta.slice(i, i+2))
    } */
 
-   this.verTodas();
-  }
+
 
   // constructor(public perfil: PerfilComponent) {}
 
@@ -55,69 +180,3 @@ export class CardComponent implements OnInit {
     //Y DESPUES EN FAVORITOS(TS DE PERFIL, EL ARRAY DONDE SE GUARDAN LAS CARDS FAVS)
     //HACEMOS .PUSH PARA AÑADIR LAS CARDS AL ARRAY
   // }
-
-  addFavoritos(id:number){
-    console.log(id);
-  }
-
-  verSoloFavoritos(){
-    //¿Qué id tiene el usuario? normalmente se guarda en localhost
-    // hacer una petición al servidor sobre que favoritos tiene
-    // con todas las ids de las prendas favoritas me las guardo en 
-    // borro respuesta y meto los favoritos en respuesta
-    
-    // borro esto
-    this.respuesta = [];
-    this.groupedItems = [];
-    // meto todas sus prendas favoritas aqui:
-    this.respuesta = [];
-
-    for(let i = 0; i < this.respuesta.length; i +=2){
-      this.groupedItems.push(this.respuesta.slice(i, i+2))
-     }
-    
-  }
-
-  verTodas(){
-    this.respuesta = [
-      {
-        id: 1,
-        titulo: "Traje boda",
-        imagen: "../../../assets/cards/chica_azul.jpg",
-        precio: 20
-      },
-      {
-        id: 2,
-        titulo: "Traje boda",
-        imagen: "../../../assets/cards/chica_morado.png",
-        precio: 20
-      },
-      {
-        id: 3,
-        titulo: "Traje boda",
-        imagen: "../../../assets/cards/chica_rosa.png",
-        precio: 20
-      },
-      {
-        id: 4,
-        titulo: "Traje boda",
-        imagen: "../../../assets/cards/chico_blanco.png",
-        precio: 20
-      },
-  
-    ];
-     /* this.groupedItems = []; */
-    // meto todas sus prendas aqui:
-    /* this.respuesta = []; */
-    for(let i = 0; i < this.respuesta.length; i +=2){
-      this.groupedItems.push(this.respuesta.slice(i, i+2))
-     }
-  }
-
-
-//PARA DETALLE-CARD
-  public verDetallePrenda(id:number){
-    /* detalle-prenda */
-    this.router.navigate(['/detalle-prenda', id]);
-  }
-}
