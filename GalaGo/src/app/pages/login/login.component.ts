00ccc74cc2e637bc91eb339df76dc40b29c3e22d
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-09-24 13:48:17 +0200

    Retos

--- GalaGo/src/app/pages/login/login.component.ts
+++ GalaGo/src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 
 @Component({
@@ -10,12 +11,16 @@ import { User } from 'src/app/models/user';
 
 export class LoginComponent {
   public login: User
-  constructor(){
+  public myForm: FormGroup
+  constructor(private route: Router){
     this.login =new User()
   }
   onSubmit(form:NgForm){
     console.log(form.value);
     console.log(this.login);
     
+  }
+  public loginHome(){
+    this.route.navigate(['/home']);
   }
     }
