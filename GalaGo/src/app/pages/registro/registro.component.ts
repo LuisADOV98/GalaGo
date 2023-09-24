17521b7b29c1704ceeff16f3b5ff7544d39a127d
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-09-25 01:36:34 +0200

    Retos

--- GalaGo/src/app/pages/registro/registro.component.ts
+++ GalaGo/src/app/pages/registro/registro.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Prenda } from 'src/app/models/prenda';
 import { User } from 'src/app/models/user';
 
 @Component({
@@ -11,9 +12,18 @@ import { User } from 'src/app/models/user';
 
 export class RegistroComponent {
   // public register: User
+  public prendas:Prenda[]
+
   public myForm: FormGroup
+  public arrUbicacion:string[] = ["Madrid","Barcelona","Badajoz","avila"]
+  public prenda:Prenda
+
   constructor(private formBuilder: FormBuilder, private route: Router){
     this.buildForm();
+    this.prendas = []
+
+    this.prenda = new Prenda("",0,"","","","","","")
+
   }
   // onSubmit(form: NgForm) {
   //   console.log(form.value);
@@ -33,6 +43,7 @@ export class RegistroComponent {
       url: [, Validators.required],
       password: [, [Validators.required, Validators.minLength(minPassLength)]],
       password2: [, [Validators.required, this.checkPassword]],
+      ubicacion2:[,Validators.required]
 
     })
   }
