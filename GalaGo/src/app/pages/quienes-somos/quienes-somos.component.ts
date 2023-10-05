462b615d93ecbb34f0efcdfd04f347cbf24d7403
LuisADOV98 <luisadov26@gmail.com>
2023-10-06 01:37:09 +0200

    maquetacion

--- GalaGo/src/app/pages/quienes-somos/quienes-somos.component.ts
+++ GalaGo/src/app/pages/quienes-somos/quienes-somos.component.ts
@@ -13,11 +13,11 @@ export class QuienesSomosComponent {
   constructor(private userService: UserService){
 
     this.tarjetas = [
-                      new Desarrolladores("Janet Rojas", "Full Stack Developer", "../../../assets/quienesSomos/janet.png", "../../../assets/quienesSomos/github-512 2.png"),
-                      new Desarrolladores("Mabel San Román", "Full Stack Developer", "../../../assets/quienesSomos/mabel.jpeg", "../../../assets/quienesSomos/github-512 2.png"),
-                      new Desarrolladores("Luis De Oliveira", "Full Stack Developer", "../../../assets/quienesSomos/luis 2.jpg", "../../../assets/quienesSomos/github-512 2.png"),
-                      new Desarrolladores("Cristina Muñoz", "Full Stack Developer", "../../../assets/quienesSomos/cristina.jpg", "../../../assets/quienesSomos/github-512 2.png"),
-                      new Desarrolladores("Juan Ángel", "Full Stack Developer", "../../../assets/quienesSomos/juanA.jpg", "../../../assets/quienesSomos/github-512 2.png"),
+                      new Desarrolladores("Janet Rojas", "Full Stack Developer", "../../../assets/quienesSomos/janet.png", "https://github.com/JanetRW"),
+                      new Desarrolladores("Mabel San Román", "Full Stack Developer", "../../../assets/quienesSomos/mabel.jpeg", "https://github.com/masadepizza"),
+                      new Desarrolladores("Luis De Oliveira", "Full Stack Developer", "../../../assets/quienesSomos/luis 2.jpg", "https://github.com/LuisADOV98"),
+                      new Desarrolladores("Cristina Muñoz", "Full Stack Developer", "../../../assets/quienesSomos/cristina.jpg", "https://github.com/cmr68"),
+                      new Desarrolladores("Juan Ángel", "Full Stack Developer", "../../../assets/quienesSomos/juanA.jpg", "https://github.com/Fantasma542"),
 
     ]
 
