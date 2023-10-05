ea99fcb31a2f5d7e3701abd653f550191a7edaee
Mabel <mabelsanromanf@gmail.com>
2023-10-05 14:05:44 +0200

    cerrar menu clicando pantalla

--- GalaGo/src/app/component/header/header.component.ts
+++ GalaGo/src/app/component/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/shared/user.service';
-// import { UserService } from 'src/app/shared/user.service';
+import { ElementRef, HostListener } from '@angular/core';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,18 +11,20 @@ export class HeaderComponent implements OnInit{
   
   public verSubMenu = false;
 
-  //cuando esta registrado/logueado en true//
-  // public esUsuarioRegistrado = true;
-
- //cuando NO esta registrado/logueado en FALSE/
-// esUsuarioRegistrado = false;
-
-  constructor(public userService: UserService){
+  constructor(public userService: UserService, private el: ElementRef){
     
   }
 
   get isLoggedIn():boolean{
     return this.userService.logueado;
+  }
+
+  @HostListener('document:click', ['$event'])
+    onClick(event: Event): void {
+  // Compruba si el menú está abierto y si se ha clicado fuera de el
+      if (this.verSubMenu && !this.el.nativeElement.contains(event.target)) {
+        this.mostrarOcultarMenu();
+    }
   }
 
 
